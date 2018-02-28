deb76b26b2b656114cc2465b4a9fe9d494478ae2
Abisoye Falabi <abisoye@outlook.com>
2018-02-28 10:52:03 +0100

    Add project files.

---
+++ AlphaDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlphaDemo.MvcApplication" Language="C#" %>
---
+++ AlphaDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlphaDemo.Startup))]
+namespace AlphaDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
